9b0e74f37a00b20430021e8165d3945c451e1ed2
SealsRock12 <git@mail.toad39.dev>
2021-12-03 15:32:49 -0800

    Preact compat, sass

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname + "./src"),
-    },
-  },
+      react: "preact-compat",
+      "react-dom": "preact-compat"
+    }
+  }
 });
