944b513cc4cd9af284ac1fbbc72828f4242231bc
SealsRock12/Toad39 <git@mail.toad39.dev>
2022-01-01 15:15:20 -0800

    Fix code block responsiveness

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -120,7 +120,7 @@ export default function Play() {
                     {...props}
                   />
                 ) : (
-                  <code className={className} {...props}>
+                  <code className={`${className} ${styles.code}`} {...props}>
                     {children}
                   </code>
                 );
