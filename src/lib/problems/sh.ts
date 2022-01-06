eed104e18b50ed6fe5af69330a6f88ddbf01555c
SealsRock12 <git@mail.toad39.dev>
2022-01-06 14:47:31 -0800

    Fix shell problem

--- src/lib/problems/sh.ts
+++ src/lib/problems/sh.ts
@@ -24,16 +24,18 @@ The third case is also valid as quotes are often optional in Bash.'
       "The answer is (C). Located in `~/.bashrc`, this file is often used for variables, functions, aliases, etc. that we want to use in every shell instance."
   },
   {
-    problem: "How do get arguments from a shell script?",
+    problem: "How do you get arguments from a shell script?",
     options: [
       "Using `$1`, `$2`, `$3` ... `$9`, `$10`",
       "Using `$1`, `$2`, `$3` ... `${9}`, `${10}`",
       "Using `{1}`, `{2}`, `{3}` ... `{9}`, `{10}`",
-      "Using `(1)`, `(2)`, `(3)` ... `(9)`, `(10)`"
+      "Using `$0`, `$1`, `$2` ... `$9`, `${10}`"
     ],
     answer: 1,
     solution:
-      "The answer is (B). We use the dollar signs to reference the `n`th variable. It must be surrounded in braces if it is over 10."
+      "The answer is (B). We use the dollar signs to reference the `n`th variable. \
+      It must be surrounded in braces if it is over 10. In addition, $0 returns the \
+      name of the file being called, so it is not an argument."
   }
 ];
 
