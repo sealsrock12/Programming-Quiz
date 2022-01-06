99f6a8d2206e0658f1d6588542ebf24b0bc44606
SealsRock12 <git@mail.toad39.dev>
2022-01-06 14:56:50 -0800

    Add ccpp problem

--- src/lib/problems/ccpp.ts
+++ src/lib/problems/ccpp.ts
@@ -29,7 +29,30 @@ return 0;
     ],
     answer: 2,
     solution:
-      "The answer is (C), because accessing memory out of bounds will cause the program to crash. Here, we're accessing a character past the last index (which is 'd')."
+      "Accessing memory out of bounds will cause the program to crash. Here, we're accessing a character \
+past the last index (which is 'd')."
+  },
+  {
+    problem: "In C, how do you get arguments in the command line?",
+    options: [
+      "Using the `getargs()` function",
+      "Using the `argc` and `argv` parameters in main()",
+      "Using the `args` vector in main",
+      "Using the `optsc` and `optsv` parameters in main()"
+    ],
+    answer: 1,
+    solution: `In the main function, you can use the \`argc\` and \`argv\` parameters in main() to get
+the arguments. As the name suggests, \`argc\` is the number of arguments, and \`argv\` is an array of them.
+Be careful, however, because argv[0] will be the path of the executable being called:
+~~~clike
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  for (int i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
+}
+~~~`
   }
 ];
 
