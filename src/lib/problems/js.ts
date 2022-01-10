d6d6d3eba8f723cac762483911e7cc4741afa7a4
CubingSoda <82660497+CubingSoda@users.noreply.github.com>
2022-01-10 09:49:03 -0600

    remove trailing commas from js.ts, set trailing comma option to none in settings.json

--- src/lib/problems/js.ts
+++ src/lib/problems/js.ts
@@ -5,10 +5,10 @@ const jsProblems: Problem[] = [
       "Prints text on the site",
       "Prints text to the console",
       "Error message in the console",
-      "None of the above",
+      "None of the above"
     ],
     answer: 1,
-    solution: "`console.log()` prints text on to the console.",
+    solution: "`console.log()` prints text on to the console."
   },
   {
     problem:
@@ -16,7 +16,7 @@ const jsProblems: Problem[] = [
     options: ["var/let", "const/let", "const/var", "None of the above"],
     answer: 0,
     solution:
-      "`const` will make it a constant (un-changeable). So it will be the other two.",
+      "`const` will make it a constant (un-changeable). So it will be the other two."
   },
   {
     problem: "What are the states of a JavaScript promise?",
@@ -24,10 +24,10 @@ const jsProblems: Problem[] = [
       "Pending, Finished, Rejected",
       "Pending, Fulfilled, Denied",
       "Pending, Fulfilled, Rejected",
-      "Pending, Complete, Rejected",
+      "Pending, Complete, Rejected"
     ],
     answer: 2,
-    solution: "They are: Pending, Fulfilled, Rejected",
+    solution: "They are: Pending, Fulfilled, Rejected"
   },
   {
     problem: `What is the result of this code?
@@ -48,7 +48,7 @@ test();
     options: ["undefined, 2", "1, 2", "1, undefined", "undefined, undefined"],
     answer: 0,
     solution:
-      "Since `a` was defined *after* it was called, it returned `undefined`. However, functions in JS can be defined anywhere, so `foo()` returned `2`.",
+      "Since `a` was defined *after* it was called, it returned `undefined`. However, functions in JS can be defined anywhere, so `foo()` returned `2`."
   },
   {
     problem: "What is the difference between `undefined` and `null`?",
@@ -56,11 +56,11 @@ test();
       "`undefined` completely the same as `null`",
       "`undefined` is when no value was assigned. `null` is when there is no value, but was purposely assigned by the code",
       "`undefined` is when no value was assigned. `null` is when there is no value, but was purposely assigned by the programer",
-      "None of the above",
+      "None of the above"
     ],
     answer: 2,
     solution:
-      "`undefined` is when no value is assigned to something. The value will never be `null` unless the programmer explicity choose to make it that value. ",
+      "`undefined` is when no value is assigned to something. The value will never be `null` unless the programmer explicity choose to make it that value. "
   },
   {
     problem: `What is the result of this code?
@@ -83,7 +83,7 @@ console.log(shape.perimeter());
 
 With arrow functions, the \`this\` keyword refers to its current surrounding scope, unlike regular functions! This means that when we call \`perimeter\`, it doesn't refer to the shape object, but to its surrounding scope (window for example). 
 
-There is no value \`radius\` on that object, which returns \`NaN\`.`,
+There is no value \`radius\` on that object, which returns \`NaN\`.`
   },
   {
     problem: `What is the output?
@@ -95,12 +95,12 @@ There is no value \`radius\` on that object, which returns \`NaN\`.`,
       "`1` and `false`",
       "`false` and `NaN`",
       "`false` and `false`",
-      "`true`, `false`",
+      "`true`, `false`"
     ],
     answer: 0,
     solution: `The unary plus tries to convert an operand to a number. true is 1, and false is 0.
 
-    The string 'Lydia' is a truthy value. What we're actually asking, is "is this truthy value falsy?". This returns false.`,
+    The string 'Lydia' is a truthy value. What we're actually asking, is "is this truthy value falsy?". This returns false.`
   },
   {
     problem: `Which one is true?
@@ -118,7 +118,7 @@ const mouse = {
       "`mouse.bird.size` is not valid",
       "`mouse[bird.size]` is not valid",
       '`mouse[bird["size"]]` is not valid',
-      "All of them are valid",
+      "All of them are valid"
     ],
     solution: `In JavaScript, all object keys are strings (unless it's a Symbol). Even though we might not *type* them as strings, they are always converted into strings under the hood.
 
@@ -126,7 +126,7 @@ JavaScript interprets (or unboxes) statements. When we use bracket notation, it 
 
 \`mouse[bird.size]\`: First it evaluates \`bird.size\`, which is \`"small"\`. \`mouse["small"]\` returns \`true\`.
 
-However, with dot notation, this doesn't happen. \`mouse\` does not have a key called \`bird\`, which means that \`mouse.bird\` is \`undefined\`. Then, we ask for \`size\` using dot notation: \`mouse.bird.size\`. Since \`mouse.bird\` is \`undefined\`, we're actually asking \`undefined.size\`. This isn't valid, and will throw an error similar to \`Cannot read property "size" of undefined\`.`,
+However, with dot notation, this doesn't happen. \`mouse\` does not have a key called \`bird\`, which means that \`mouse.bird\` is \`undefined\`. Then, we ask for \`size\` using dot notation: \`mouse.bird.size\`. Since \`mouse.bird\` is \`undefined\`, we're actually asking \`undefined.size\`. This isn't valid, and will throw an error similar to \`Cannot read property "size" of undefined\`.`
   },
   {
     problem: `What is the output?
@@ -143,8 +143,8 @@ console.log(d.greeting);
 
 First, variable \`c\` holds a value to an object. Later, we assign \`d\` with the same reference that \`c\` has to the object.
 
-When you change one object, you change all of them.`,
-  },
+When you change one object, you change all of them.`
+  }
 ];
 
 export default jsProblems;
