32b686920e932e1e4bed37d901f84c5c3383d508
CubingSoda <CleverProgrammer2021@outlook.com>
2022-01-17 20:57:21 -0600

    added several js problems, made a system that automiatcally generates the latest problem

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -17,6 +17,12 @@ import ReportErrorModal from "@/components/ReportErrorModal";
 import { AppContext } from "@/components/AppProvider";
 // import AdsModal from "@/components/AdsModal";
 
+/* 
+  For developing problems.
+  The program will generate the last object in lib/problems/<lang>.ts
+*/
+const PROBLEM_DEV_LANG = "js";
+
 export default function Play() {
   const { problemType } = useContext(AppContext);
 
@@ -66,6 +72,15 @@ export default function Play() {
   }
 
   const [problemInfo, setProblemInfo] = useState<PopulatedProblem>(() => {
+    if (typeof PROBLEM_DEV_LANG !== "undefined") {
+      console.log("adasdfadff");
+
+      const newObject = problems[PROBLEM_DEV_LANG].at(-1);
+      newObject!["id"] = "dev-problem";
+      newObject!["lang"] = PROBLEM_DEV_LANG;
+      return newObject;
+    }
+
     if (localStorage.getItem("lang") && localStorage.getItem("id")) {
       if (localStorage.getItem("onSolution") === "true") {
         localStorage.removeItem("onSolution");
